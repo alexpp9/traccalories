c3077bdf8afd041b80be6154b54154632f4053c4
alexpp9 <alexandrup.pirtac@outlook.com>
2024-06-26 10:43:36 +0100

    Added TrackCalories, Meal, Workout classes

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// Tracker
+class CalorieTracker {
+  constructor() {
+    // Make properties private
+    this._calorieLimit = 2000;
+    this._totalCalories = 0;
+    this._meals = [];
+    this._workouts = [];
+  }
+  // Adds meal to meals array - meal shall be an object;
+  addMeal(meal) {
+    this._meals.push(meal);
+    // Update total calories by number of calories from meal;
+    this._totalCalories += meal.calories;
+  }
+  addWorkout(workout) {
+    this._workouts.push(workout);
+    // Substract calories "burned";
+    this._totalCalories -= workout.calories;
+  }
+}
+
+// Meal Class
+class Meal {
+  constructor(name, calories) {
+    // Create random id
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
+}
+// Workout Class
+class Workout {
+  constructor(name, calories) {
+    // Create random id
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
+}
+
+// Initialize tracker
+const tracker = new CalorieTracker();
+const breakfast = new Meal('Breakfast', 400);
+tracker.addMeal(breakfast);
+
+const run = new Workout('Morning run', 300);
+tracker.addWorkout(run);
+
+console.log(tracker._meals);
+console.log(tracker._workouts);
+console.log(tracker._totalCalories);
